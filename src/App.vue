2f300c295126a525ea1e9ff1ed597e800bd63ef3
ChenYu <ycyplus@gmail.com>
2025-04-28 14:22:58 +0800

    feat(dynamicRouter.json): dynamicRouter json data config finish

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
  * @Author: ChenYu ycyplus@gmail.com
  * @Date: 2025-03-30 17:45:29
  * @LastEditors: ChenYu ycyplus@gmail.com
- * @LastEditTime: 2025-04-03 14:55:38
- * @FilePath: \bun_vite_uno_naive\src\App.vue
+ * @LastEditTime: 2025-04-28 10:20:34
+ * @FilePath: \Robot_Admin\src\App.vue
  * @Description: 根入口文件
  * Copyright (c) 2025 by CHENY, All Rights Reserved 😎.
 -->
--- src/router/publicRouter.ts
+++ src/router/publicRouter.ts
@@ -2,47 +2,68 @@
  * @Author: ChenYu ycyplus@gmail.com
  * @Date: 2025-04-27 22:11:48
  * @LastEditors: ChenYu ycyplus@gmail.com
- * @LastEditTime: 2025-04-27 23:35:52
+ * @LastEditTime: 2025-04-28 11:18:25
  * @FilePath: \Robot_Admin\src\router\publicRouter.ts
  * @Description: 静态路由表
  * Copyright (c) 2025 by CHENY, All Rights Reserved 😎.
  */
 import type { RouteRecordRaw } from 'vue-router'
 
-/**
- * TODO: 公开路由表
- */
 export const publicRoutes: RouteRecordRaw[] = [
-  // {
-  //   path: '/C_Container',
-  //   name: 'C_Container',
-  //   component: () => import('@/components/global/C_Container/index.vue'),
-  //   meta: { hidden: true },
-  // },
   {
     path: '/login',
+    name: 'login',
     component: () => import('_views/login/index.vue'),
-    meta: { hidden: true },
+    meta: {
+      title: '登录',
+      icon: '',
+      link: '',
+      hidden: true,
+      full: false,
+      affix: false,
+      keepAlive: false,
+    },
   },
   {
     path: '/401',
     name: '401',
     component: () => import('_views/error-page/401.vue'),
-    meta: { hidden: true },
+    meta: {
+      title: '无权限',
+      icon: '',
+      link: '',
+      hidden: true,
+      full: false,
+      affix: false,
+      keepAlive: false,
+    },
   },
-
-  // 用来解决初次加载没有路由console提示Vue Router warn]: No match found for location with path的问题
   {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
     component: () => import('_views/error-page/404.vue'),
-    meta: { hidden: true },
+    meta: {
+      title: '404',
+      icon: '',
+      link: '',
+      hidden: true,
+      full: false,
+      affix: false,
+      keepAlive: false,
+    },
   },
-
-  // 临时空白的路径，解决开发环境本地警告
   {
     path: '/:catchAll(.*)',
     component: () => import('_views/error-page/blank.vue'),
+    meta: {
+      title: '空白',
+      icon: '',
+      link: '',
+      hidden: true,
+      full: false,
+      affix: false,
+      keepAlive: false,
+    },
   },
 ]
 
