84b2e59cbe321ec94b61b05156b034f47f36ab14
ChenYu <ycyplus@gmail.com>
2025-09-04 11:16:32 +0800

    wip(11-table-expand): add api interface

---
+++ src/api/11-table-expand.ts
@@ -0,0 +1,9 @@
+import { getData } from '@/axios/request'
+import type { GetEmployeesExpandListResponse } from './generated'
+
+/**
+ * * @description: 获取员工展开列表接口
+ * ! @return {Promise<GetEmployeesExpandListResponse>} 员工展开列表响应数据，包含嵌套的项目/需求/服务信息
+ */
+export const getEmployeesExpandListApi = () =>
+  getData<GetEmployeesExpandListResponse>('/employees/expandList')
